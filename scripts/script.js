021bcd6717f86a654cbfd020c19f0d15453d4e62
RH0426 <reidman426@gmail.com>
2026-09-23 13:49:56 -0400

    1.8 and 1.9
    tables and forms

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+// this is a single line comment and is ignored by the browser
+
+/*
+this is a 
+multi-line 
+comment
+*/
+
+console.log(10)       // same as print() in python
+
+console.log("Reid Hughes", 16)
+console.warn("this is a warning")
+console.error("This is an error")
